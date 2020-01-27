9acc32c57d1af3f050645882c7f7b9c3c59e4b58
Max Levitskiy <max.dstu@gmail.com>
2020-01-27 19:57:22 +0100

    traefik - helm 3 for install.sh, updated values.yaml

--- loadbalancer/traefik/helm/install.sh
+++ loadbalancer/traefik/helm/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 read -p "digital ocean api key: " dotoken
 kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
-helm install stable/traefik --name traefik --namespace kube-public -f values.yaml --set acme.dnsProvider.digitalocean.DO_AUTH_TOKEN=${dotoken}
+helm install traefik stable/traefik --namespace kube-public -f values.yaml --set acme.dnsProvider.digitalocean.DO_AUTH_TOKEN=${dotoken}

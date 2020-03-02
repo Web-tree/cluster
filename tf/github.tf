5a2689dfa30d17a791c602b8e007e4f8a4a47f25
Max Levitskiy <max.dstu@gmail.com>
2020-03-02 08:20:34 +0100

    github branch protection update

--- tf/github.tf
+++ tf/github.tf
@@ -14,18 +14,23 @@ resource "github_branch_protection" "master_branch" {
     ]
   }
   restrictions {
-    teams = ["owners"]
+    teams = [
+      "@Web-tree/tools",
+      "@Web-tree/owners"
+    ]
   }
 }
 
 resource "github_branch_protection" "ghp_branch" {
   count = length(var.github-ghp-branch-protection)
   repository     = var.github-ghp-branch-protection[count.index]
-  branch         = "master"
+  branch         = "gh-pages"
   required_status_checks {
     strict   = true
   }
   restrictions {
-    teams = ["tools"]
+    teams = [
+      "@Web-tree/tools",
+    ]
   }
 }
